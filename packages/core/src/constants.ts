4decfd0dc6961823574e2211d21318a45bcb1cd7
Saurabh Prakash <saurabhp@lambdatest.com>
2022-05-25 15:21:25 +0530

    Added option to persist jest cache

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -1,4 +1,4 @@
-export const TAS_DIRECTORY = process.cwd() + "/__tas";
+export const TAS_DIRECTORY = process.env.REPO_CACHE_DIR ?? (process.cwd() + "/__tas");
 export const SMART_INPUT_FILE = TAS_DIRECTORY + "/input.json";
 export const SMART_OUT_FILE = TAS_DIRECTORY + "/out.json";
 export const LocatorSeparator = "##";
